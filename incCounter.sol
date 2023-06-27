97f6a2e2d916fa096c5ee355c191b2b49c48d272
Chainboxcode <137642867+chainboxcode@users.noreply.github.com>
2023-06-27 04:51:38 +0100

    Update incCounter.sol
    Code for the revert statement.

--- incCounter.sol
+++ incCounter.sol
@@ -1,1 +1,46 @@
-Initial.
+// SPDX-License-Identifier: BSD-2-Clause
+pragma solidity ^0.8.16;
+
+// This code increments the counter
+// In the error code, the counter is incremented until the value 
+// and the txn is reverted, so the counter never reaches 3
+// The working code places an inequality to test for 3 or tests the value before in the increment.
+
+contract Counter {
+    address private owner;
+    uint256 private counter;
+
+    constructor() {
+        owner = msg.sender;
+        counter = 0;
+    }
+
+    // Maximum Value of counter should be 3
+    function incErr() external {
+        require(msg.sender == owner, "Unauthorized");
+        counter++;
+        if (counter == 3) {
+            revert("Counter reached the max value");
+        }
+    }
+
+    function incEq() external {
+        require(msg.sender == owner, "Unauthorized");
+    
+        if (counter == 3) {
+            revert("Counter reached the max value");
+        }
+        counter++;
+    }
+    function incGr() external {
+        require(msg.sender == owner, "Unauthorized");
+        counter++;
+        if (counter > 3) {
+            revert("Counter reached the max value");
+        }
+    }
+
+    function getCounter() external view returns (uint256) {
+        return counter;
+    }
+}
