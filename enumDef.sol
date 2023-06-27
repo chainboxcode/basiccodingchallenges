da906002b0087dd99459d78b193ccc823f6a6b4a
Chainboxcode <137642867+chainboxcode@users.noreply.github.com>
2023-06-27 09:11:16 +0100

    Update enumDef.sol
    defaults

--- enumDef.sol
+++ enumDef.sol
@@ -1,1 +1,13 @@
-enum
+// SPDX-License-Identifier: BSD-2-Clause
+pragma solidity ^0.8.16;
+
+contract Enum {
+    enum Status { 
+        Pending, 
+        Shipped, 
+        Accepted, 
+        Rejected, 
+        Canceled 
+    }
+    Status public status;
+}
