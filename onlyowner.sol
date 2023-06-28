442d46fd91cfb042ec5545aaa19e129b19bc53a9
Chainboxcode <137642867+chainboxcode@users.noreply.github.com>
2023-06-28 04:39:17 +0100

    Update onlyowner.sol
    onlyowner

--- onlyowner.sol
+++ onlyowner.sol
@@ -1,1 +1,38 @@
-ddd
+pragma solidity ^0.8.7;
+
+contract Owner {
+    address public owner;
+    uint private code;
+
+    /* 
+       This code illustrates the changeOwnerInsecure function and the absence of onlyOwner which would
+       anyone to change owner status. This was a problem in early solidity programs
+       which was exploited to mint ERC20 tokens by anyone calling the contract.
+    */
+
+    constructor(address _owner, uint _code) {
+        owner = _owner;
+        code = _code;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function.");
+        _;
+    }
+
+    function getOwner() public view returns (address) {
+        return owner;
+    }
+
+    function changeOwner(address _newOwner, uint _newCode) public onlyOwner {
+        require(_newCode == code, "Please enter the correct code!");
+
+        owner = _newOwner;
+    }
+
+    function changeOwnerInsecure(address _newOwner, uint _newCode) public  {
+        require(_newCode == code, "Please enter the correct code!");
+
+        owner = _newOwner;
+    }
+}
